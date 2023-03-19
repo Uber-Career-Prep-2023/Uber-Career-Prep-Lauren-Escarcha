7502fbff70d353cdb1c78cc683fad393bab1a1c6
Lauren Escarcha <laurenescarcha@ufl.edu>
2023-03-19 07:19:25 -0400

    updated notes

--- Assignment 1/BackspaceStringCompare.cpp
+++ Assignment 1/BackspaceStringCompare.cpp
@@ -11,13 +11,13 @@ public:
     void BackspaceStringCompare(string input1, string input2) {
 
         //count for original string
-        int indexHolder = 0;
         int backspaceCount = 0;
         string final1 = "";
         string final2 = "";
 
         //for first input
-        for(int k = 0; k < input1.size(); k++){
+        for(int k = input1.size() - 1; k >= 0; k--){
+
             if(input1[k] == '#'){
                 //update # count
                 //don't add to finalize string
@@ -25,34 +25,21 @@ public:
             } 
             else if(backspaceCount > 0 && input1[k] != '#'){
                 //letter after backspace is found
-                //delete corresponding letters before the #
-                
-                //final1 should have letter, if not, then its a stray #
-                if(final1.empty()|| final1.size() < backspaceCount){
-                    cout << "False";
-                    return;
-                }
-
-                //(starting point, length)    
-                final1.erase(final1.size() - (backspaceCount), (backspaceCount));
-                //reset back count
-                backspaceCount = 0;
-
-
-                //be sure to actually add letter to string
-                final1 += input1[k];
+                //skip corresponding letters before the #
+                backspaceCount--;
             } else {
                 // just add to finalized string
                 final1 += input1[k];
             }
         }
         
-        //  cout << final1 << endl;
+        //  cout << final1 << " ";
 
         //for second input
         backspaceCount = 0;
 
-        for(int k = 0; k < input2.size(); k++){
+        //for first input
+        for(int k = input2.size() - 1; k >= 0; k--){
             if(input2[k] == '#'){
                 //update # count
                 //don't add to finalize string
@@ -60,29 +47,15 @@ public:
             } 
             else if(backspaceCount > 0 && input2[k] != '#'){
                 //letter after backspace is found
-                //delete corresponding letters before the #
-                
-                //final1 should have letter, if not, then its a stray #
-                if(final2.empty() || final2.size() < backspaceCount){
-                    cout << "False";
-                    return;
-                }
-
-                //(starting point, length) 
-                final2.erase(final2.size() - (backspaceCount), (backspaceCount));
-                //reset back count
-                backspaceCount = 0;
-
-
-                //be sure to actually add letter to string
-                final2 += input2[k];
+                //skip corresponding letters before the #
+                backspaceCount--;
             } else {
                 // just add to finalized string
                 final2 += input2[k];
             }
         }
 
-        // cout << final2;
+        // cout << final2 << endl;
 
         if(final1 == final2){
             cout << "True";
@@ -100,26 +73,33 @@ int main(){
 
     Solution solution;
 
+    //true
     solution.BackspaceStringCompare("abcde", "abcde");
     cout << ": abcde, abcde";
     cout << "\n";
 
+    //true
     solution.BackspaceStringCompare("Uber Career Prep", "u#Uber Careee#r Prep");
     cout << ": Uber Career Prep, u#Uber Careee#r Prep";
     cout << "\n";
 
+
+    //true
     solution.BackspaceStringCompare("abcw#xyz", "abcdef###xyz");
     cout << ": abcdef###xyz, abcw#xyz";
     cout << "\n";
 
+    //false
     solution.BackspaceStringCompare("abcdef###xyz", "abcdefxyz###");
     cout << ": abcdef###xyz, abcdefxyz###";
     cout << "\n";
 
+    //true
     solution.BackspaceStringCompare("uu##urmo#o#om", "urmom");
     cout << ": uu##urmo#o#om, urmom";
     cout << "\n";
 
+    //true
     solution.BackspaceStringCompare("uu###urmo#o#om", "urmom");
     cout << ": uu###urmo#o#om, urmom";
     cout << "\n";
