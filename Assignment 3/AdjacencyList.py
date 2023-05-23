183e768e1b00e07c89d27c16d4c9d2838cc7199d
Lauren Escarcha <laurenescarcha@ufl.edu>
2023-05-22 22:46:48 -0400

    DFS done

--- Assignment 3/AdjacencyList.py
+++ Assignment 3/AdjacencyList.py
@@ -2,22 +2,22 @@ class AdjacencyList:
     def __init__(self):
         pass
 
-    #global graph = map of sets
-    graph = {}
+    #global variables 
+    graph = {} #graph = map of sets
+    vertices = 0 #num of vert
 
 
     # --- TIME COMPLEXITY: O(V+E)
     # --- SPACE COMPLEXITY: O(V^2), where every node is connected to every other node
-    #edges = list of pairs
     def buildGraph(self, edges):
 
         #loops thru each pair and makes connection
         #will only connect first 2 indexes, since it's a pair
         for pair in edges:
-            self.makeConnection(pair[0], pair[1])
+            self.buildConnection(pair[0], pair[1])
     
     #connects edge start and end
-    def makeConnection(self, start, end):
+    def buildConnection(self, start, end):
         if self.graph.get(start):
             #if key was inserted before, add new edge
             #duplicates will be ignored since edges are in a set
@@ -29,7 +29,24 @@ class AdjacencyList:
 
         if self.graph.get(end) is None:
             #if end is not a key itself, add
-            self.graph.update({end: {}})
+            self.graph.update({end: set()})
+
+    def DFS(self):
+        #starts at first node in map, can be any node
+        self.vertices = len(self.graph)
+        visited = set()
+
+        self.DFShelper(list(self.graph.keys())[0], visited)
+
+    def DFShelper(self, vertice, visited):
+        #can start at any node
+        visited.add(vertice)
+        print(vertice, " ")
+
+        for neighbor in self.graph[vertice]:
+            if neighbor not in visited:
+                self.DFShelper(neighbor, visited)
+
     
     def printGraph(self):
 
@@ -48,6 +65,8 @@ def main():
 
     graph.buildGraph([(1, 2), (2, 3), (1, 3), (3, 2), (2, 0)])
     graph.printGraph()
+
+    graph.DFS()
 
 main()
 
