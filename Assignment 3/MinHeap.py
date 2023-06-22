1b616187f1598760265b527d0ee51ca56f4b1d14
Lauren Escarcha <laurenescarcha@ufl.edu>
2023-06-22 07:26:36 -0400

    uploaded questions 2-10

---
+++ Assignment 3/MinHeap.py
@@ -0,0 +1,90 @@
+class MinHeap:
+    
+    def __init__(self):
+        self.minHeap = []
+
+    def top(self):
+        return self.heap[0]
+
+    def insert(self, num, minHeap):
+        #1 - add element to end (rightmost of last level)
+        self.minHeap.append(num)
+
+        #index of inserted num and given parents
+        child = len(self.minHeap) - 1
+        parent = (child-1)/2
+
+        #HEAPIFY UP:
+        #while not root
+        #and parent is LESS than child
+        while(child > 0 and self.minHeap[parent] > self.minHeap[child]):
+            #swap 
+            self.heap[parent],self.heap[child] = self.heap[child],self.heap[parent]
+            child, parent = parent, (child-1)/2
+
+        #TODO: do I need this or can I just call heapify^?
+
+    def delete(self):
+        #edge case
+        if self.minHeap is None or len(self.minHeap) == 0:
+            return
+
+        #1 - move last element to root (and delete og root value)
+        self.minHeap[0] = self.minHeap[len(self.minHeap)-1]
+
+        #2 - delete the last index, decrease size
+        self.minHeap.pop()
+
+        #3 - heapify to maintain order
+        self.heapify(0)
+
+
+    #build heap from array = start from back of array
+    #delete node in heap = start from beginning of array
+    def heapify(self, i):
+
+        left = 2*i+1
+        right = 2*i+2
+        smallest = i
+
+        # if left is in bounds and smaller
+        if left < len(self.minHeap) and self.minHeap[left] < self.minHeap[smallest]:
+            smallest = left
+        
+        #same ^ but comparing w updated smallest index
+        if right < len(self.minHeap) and self.minHeap[right] < self.minHeap[smallest]:
+            smallest = right
+
+        #if smallest is NOT parent, swap and continue with subtree
+        if smallest != i:
+            print("smallest: ", self.minHeap[smallest])
+            self.minHeap[i],self.minHeap[smallest] = self.minHeap[smallest],self.minHeap[i]
+            self.heapify(smallest)
+
+    #builds heap from given array
+    def buildHeap(self, arr):
+        self.minHeap = arr
+
+        #start from last index in array
+        for i in range(len(arr), -1, -1):
+            self.heapify(i)
+
+        print()
+
+
+    #builds heap from given array
+    def printHeap(self):    
+        for i in self.minHeap:
+            print(i, end=" ")
+
+def main():
+
+    arr = [1,5,6,8,9,7,3] # 3 and 6 are suppose to swap
+    myHeap = MinHeap()
+
+    myHeap.buildHeap(arr)
+    myHeap.printHeap()
+
+    return 0
+
+main()
